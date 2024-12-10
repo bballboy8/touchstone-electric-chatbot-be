cd3e24043117c99757a0231cb0d8f8a5efb2e8d4
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-12-10 15:14:05 +0530

    Update train_agent_service.py

--- app/services/train_agent_service.py
+++ app/services/train_agent_service.py
@@ -33,23 +33,23 @@ async def extract_useful_pages(pdf_path, min_words=20, skip_keywords=None):
 
 async def chunk_text(text, max_tokens=900):
     try:
-        sentences = sent_tokenize(text)
-        chunks = []
-        current_chunk = []
-        current_length = 0
+        # sentences = sent_tokenize(text)
+        # chunks = []
+        # current_chunk = []
+        # current_length = 0
 
-        for sentence in sentences:
-            sentence_length = len(sentence.split())
-            if current_length + sentence_length > max_tokens:
-                chunks.append(" ".join(current_chunk))
-                current_chunk = []
-                current_length = 0
-            current_chunk.append(sentence)
-            current_length += sentence_length
+        # for sentence in sentences:
+        #     sentence_length = len(sentence.split())
+        #     if current_length + sentence_length > max_tokens:
+        #         chunks.append(" ".join(current_chunk))
+        #         current_chunk = []
+        #         current_length = 0
+        #     current_chunk.append(sentence)
+        #     current_length += sentence_length
 
-        if current_chunk:
-            chunks.append(" ".join(current_chunk))
-        return {"status_code": 200, "response": chunks}
+        # if current_chunk:
+        #     chunks.append(" ".join(current_chunk))
+        return {"status_code": 200, "response": text}
     except Exception as e:
         return {"status_code": 500, "response": f"Error while chunking text: {e}"}
 
