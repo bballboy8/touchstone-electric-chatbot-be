f4b2e8e93d7996d1789990cb8b164208f6415c5f
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-12-11 13:24:53 +0530

    protected paid endpoints

--- app/controllers/health_check_controller.py
+++ app/controllers/health_check_controller.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 import services
 from logging_module import logger
+from utils.dependencies import get_current_user_id
 
 router = APIRouter()
 
@@ -8,7 +9,7 @@ router = APIRouter()
 @router.get(
     "/openai-chat-completion",
 )
-async def openai_health_chat_completion_check():
+async def openai_health_chat_completion_check(user_id: int = Depends(get_current_user_id)):
     logger.debug("Inside OpenAI health check chat completion controller")
     response = await services.test_openai_chat_completion_service()
     logger.debug("Response from OpenAI health check chat completion  controller")
@@ -17,7 +18,7 @@ async def openai_health_chat_completion_check():
 @router.get(
     "/openai-text-embeddings",
 )
-async def openai_health_text_embeddings_check():
+async def openai_health_text_embeddings_check(user_id: int = Depends(get_current_user_id)):
     logger.debug("Inside OpenAI health check Text Embeddings controller")
     response = await services.test_openai_for_text_embeddings_service()
     logger.debug("Response from OpenAI health check Text Embeddings controller")
@@ -26,7 +27,7 @@ async def openai_health_text_embeddings_check():
 @router.get(
     "/pineconedb",
 )
-async def pinecone_health_check():
+async def pinecone_health_check(user_id: int = Depends(get_current_user_id)):
     logger.debug("Inside pinecone health check controller")
     response = await services.test_pinconedb_service()
     logger.debug("Response from pinecone health check controller")
@@ -35,7 +36,7 @@ async def pinecone_health_check():
 @router.get(
     "/service-titan-api",
 )
-async def service_titan_api_health_check():
+async def service_titan_api_health_check(user_id: int = Depends(get_current_user_id)):
     logger.debug("Inside Service Titan API health check controller")
     response = await services.test_service_titan_api_service()
     logger.debug("Response from Service Titan API health check controller")
