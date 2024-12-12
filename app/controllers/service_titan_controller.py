292620f37526b1a1dfe7d060a5362bda2def9a45
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-12-12 18:16:17 +0530

    Endpoints for fetching Job Location/s and Jobs

--- app/controllers/service_titan_controller.py
+++ app/controllers/service_titan_controller.py
@@ -24,3 +24,43 @@ async def get_service_titan_customers(page: int = 1, page_size: int = 10, user_i
     response = await services.get_service_titan_customers(page, page_size)
     logger.debug("Response from Service Titan customers controller")
     return response
+
+
+@router.get(
+    "/get-jobs",
+)
+async def get_service_titan_jobs(page: int = 1, page_size: int = 10):
+    logger.debug("Inside Service Titan jobs controller")
+    response = await services.get_service_titan_jobs(page, page_size)
+    logger.debug("Response from Service Titan jobs controller")
+    return response
+
+
+@router.get(
+    "/get-job-by-id",
+)
+async def get_service_titan_job_by_id(job_id: int):
+    logger.debug("Inside Service Titan job by id controller")
+    response = await services.get_service_titan_job_by_id(job_id)
+    logger.debug("Response from Service Titan job by id controller")
+    return response
+
+
+@router.get(
+    "/get-locations",
+)
+async def get_service_titan_locations(page: int = 1, page_size: int = 10):
+    logger.debug("Inside Service Titan locations controller")
+    response = await services.get_service_titan_locations(page, page_size)
+    logger.debug("Response from Service Titan locations controller")
+    return response
+
+
+@router.get(
+    "/get-location-by-id",
+)
+async def get_service_titan_location_by_id(location_id: int):
+    logger.debug("Inside Service Titan location by id controller")
+    response = await services.get_service_titan_location_by_id(location_id)
+    logger.debug("Response from Service Titan location by id controller")
+    return response
--- app/services/service_titan_service.py
+++ app/services/service_titan_service.py
@@ -2,7 +2,6 @@ from thirdparty.service_titan_api_service import ServiceTitanApiService
 from logging_module import logger
 
 
-
 async def get_service_titan_employees(
     page: int = 1, page_size: int = 10
 ):
@@ -32,4 +31,60 @@ async def get_service_titan_customers(
         return {"status_code": 200, "data": response["data"]}
     except Exception as e:
         logger.error(f"Error getting Service Titan customers: {e}")
+        return {"status_code": 500, "data": f"Internal server error:{e}"}
+
+
+async def get_service_titan_jobs(page: int = 1, page_size: int = 10):
+    logger.info("Getting Service Titan jobs")
+    try:
+        service_titan_api_service = ServiceTitanApiService()
+        response = await service_titan_api_service.get_jobs(page, page_size)
+        logger.info("Service Titan jobs received")
+        if response["status_code"] != 200:
+            return response
+        return {"status_code": 200, "data": response["data"]}
+    except Exception as e:
+        logger.error(f"Error getting Service Titan jobs: {e}")
+        return {"status_code": 500, "data": f"Internal server error:{e}"}
+
+
+async def get_service_titan_job_by_id(job_id: int):
+    logger.info("Getting Service Titan job by id")
+    try:
+        service_titan_api_service = ServiceTitanApiService()
+        response = await service_titan_api_service.get_job_by_id(job_id)
+        logger.info("Service Titan job by id received")
+        if response["status_code"] != 200:
+            return response
+        return {"status_code": 200, "data": response["data"]}
+    except Exception as e:
+        logger.error(f"Error getting Service Titan job by id: {e}")
+        return {"status_code": 500, "data": f"Internal server error:{e}"}
+
+
+async def get_service_titan_locations(page: int = 1, page_size: int = 10):
+    logger.info("Getting Service Titan locations")
+    try:
+        service_titan_api_service = ServiceTitanApiService()
+        response = await service_titan_api_service.get_locations(page, page_size)
+        logger.info("Service Titan locations received")
+        if response["status_code"] != 200:
+            return response
+        return {"status_code": 200, "data": response["data"]}
+    except Exception as e:
+        logger.error(f"Error getting Service Titan locations: {e}")
+        return {"status_code": 500, "data": f"Internal server error:{e}"}
+
+
+async def get_service_titan_location_by_id(location_id: int):
+    logger.info("Getting Service Titan location by id")
+    try:
+        service_titan_api_service = ServiceTitanApiService()
+        response = await service_titan_api_service.get_location_by_id(location_id)
+        logger.info("Service Titan location by id received")
+        if response["status_code"] != 200:
+            return response
+        return {"status_code": 200, "data": response["data"]}
+    except Exception as e:
+        logger.error(f"Error getting Service Titan location by id: {e}")
         return {"status_code": 500, "data": f"Internal server error:{e}"}
