9e0b1315a715e232e34e7eab67ee38ebd5a14ec0
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-12-19 17:57:15 +0530

    Update botjs.js

--- static/botjs.js
+++ static/botjs.js
@@ -1,26 +1,29 @@
 Tawk_API = Tawk_API || {};
 
-Tawk_API.onLoad = function() {
-    console.log('Tawk.to widget loaded');
-    Tawk_API.onMessageSent = function(message) {
-        console.log('User sent a message:', message);
+Tawk_API.onLoad = function () {
+  console.log("Tawk.to widget loaded");
+  Tawk_API.onChatMessageVisitor = function (message) {
+    console.log("User sent a message:", message);
 
-        fetch('https://fastapi-service-728413338172.us-central1.run.app/api/client/process-tawk-query', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ message: message }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('API response:', data);
-
-            Tawk_API.addMessage({
-                text: data.response,
-                type: 'system',
-            });
-        })
-        .catch(error => console.error('Error:', error));
-    };
+    fetch(
+      "https://fastapi-service-728413338172.us-central1.run.app/api/client/process-tawk-query",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          message: message["message"],
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("API response:", data);
+        let response = data["response"];
+        console.log("Bot response:", response);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 };
