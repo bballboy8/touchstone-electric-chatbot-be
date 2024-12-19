eb25a14370da2a8cebde0fde6e1e754993972c14
Aman Khan <173542493+ballboyredditor@users.noreply.github.com>
2024-12-19 18:43:28 +0530

    Update client_agent_training_controller.py

--- app/controllers/client_agent_training_controller.py
+++ app/controllers/client_agent_training_controller.py
@@ -95,7 +95,7 @@ async def get_system_prompt():
 @router.post(
     "/process-tawk-query",
 )
-async def process_tawk_query(message: str = Form(...)):
+async def process_tawk_query(message: str):
     logger.debug("Inside Process Tawk Query controller")
     response = await services.process_tawk_query_service(message)
     logger.debug("Response from Process Tawk Query controller")
